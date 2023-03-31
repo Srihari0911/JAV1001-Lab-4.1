944b383bcae620ab4b07f3d7ea478a27435a6a96
Sri hari Sreenivasan <122223722+Srihari0911@users.noreply.github.com>
2023-03-31 18:26:41 -0400

    Updated MainActivity.kt
    Changed the main code by adding a toggle listener and modifying the onClickListeners for the buttons according to the toggle.

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,6 +1,4 @@
-/* This project was worked in group with Jobin Koodathinkal Jose (A00250653) and myself Sri Hari Sreenivasan (A00272180).
-We both worked in developing the logic and designing the layouts. I worked on Textviews along with spinner. 
-Jobin worked on Buttons and OnClickListeners. */
+/* This project was worked in group with Jobin Koodathinkal Jose (A00250653) and myself Sri Hari Sreenivasan (A00272180) */
 
 // We chose Cricket as our game and we don't have negative values. Though we chose Cricket the max value for spinner is 6.
 
@@ -10,62 +8,67 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.ToggleButton
+import com.example.scorekeepingapp.R.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        setContentView(layout.activity_main)
+
         //Initializing values
-        val team1Score = findViewById<TextView>(R.id.team1_score)
-        val team2Score = findViewById<TextView>(R.id.team2_score)
-        val increment1 = findViewById<Spinner>(R.id.score1_increment)
-        val increment2 = findViewById<Spinner>(R.id.score2_increment)
-        val team1Increase = findViewById<Button>(R.id.team1_increase)
-        val team1Decrease = findViewById<Button>(R.id.team1_decrease)
-        val team2Increase = findViewById<Button>(R.id.team2_increase)
-        val team2Decrease = findViewById<Button>(R.id.team2_decrease)
+        val team1Score = findViewById<TextView>(id.team1_score)
+        val team2Score = findViewById<TextView>(id.team2_score)
+        val increment1 = findViewById<Spinner>(id.score1_increment)
+        val increment2 = findViewById<Spinner>(id.score2_increment)
+        val teamIncrease = findViewById<Button>(id.team_increase)
+        val teamDecrease = findViewById<Button>(id.team_decrease)
+        val teamToggleButton = findViewById<Switch>(id.team_toggle_button)
 
         // Setting initial scores to 0
         team1Score.text = "0"
         team2Score.text = "0"
+        var currentTeam = 1
+        
 
-
-        // Set onClickListeners for team 1 buttons
-        //Team1 - Increase
-        team1Increase.setOnClickListener {
-            val scoreIncrement = increment1.selectedItem.toString().toInt()
-            val currentScore = team1Score.text.toString().toInt()
-            val newScore = currentScore + scoreIncrement
-            team1Score.text = newScore.toString()
+        // Added code code for toggle listener
+        val onCheckedChangeListener = teamToggleButton.setOnCheckedChangeListener { _, isChecked ->
+            currentTeam = if (isChecked) 2 else 1
         }
-        //Team1 - Decrease
-        team1Decrease.setOnClickListener {
+        // OnclickListeners to increase the values
+        teamIncrease.setOnClickListener {
             val scoreIncrement = increment1.selectedItem.toString().toInt()
-            val currentScore = team1Score.text.toString().toInt()
-            val newScore = currentScore - scoreIncrement
-            if (newScore >= 0) {
+            if (currentTeam == 1) {
+                val currentScore = team1Score.text.toString().toInt()
+                val newScore = currentScore + scoreIncrement
                 team1Score.text = newScore.toString()
+            } else {
+                val scoreIncrement = increment2.selectedItem.toString().toInt()
+                val currentScore = team2Score.text.toString().toInt()
+                val newScore = currentScore + scoreIncrement
+                team2Score.text = newScore.toString()
             }
         }
-
-        // Set onClickListeners for team 2 buttons
-        //Team2 - Increase
-        team2Increase.setOnClickListener {
-            val scoreIncrement = increment2.selectedItem.toString().toInt()
-            val currentScore = team2Score.text.toString().toInt()
-            val newScore = currentScore + scoreIncrement
-            team2Score.text = newScore.toString()
-        }
-        //Team2 - Decrease
-        team2Decrease.setOnClickListener {
-            val scoreIncrement = increment2.selectedItem.toString().toInt()
-            val currentScore = team2Score.text.toString().toInt()
-            val newScore = currentScore - scoreIncrement
-            if (newScore >= 0) {
-                team2Score.text = newScore.toString()
+        // OnclickListeners to decrease the values
+        teamDecrease.setOnClickListener {
+            val scoreIncrement = increment1.selectedItem.toString().toInt()
+            if (currentTeam == 1) {
+                val scoreIncrement = increment1.selectedItem.toString().toInt()
+                val currentScore = team1Score.text.toString().toInt()
+                val newScore = currentScore - scoreIncrement
+                if (newScore >= 0) {
+                    team1Score.text = newScore.toString()
+                }
+            } else {
+                val scoreIncrement = increment2.selectedItem.toString().toInt()
+                val currentScore = team2Score.text.toString().toInt()
+                val newScore = currentScore - scoreIncrement
+                if (newScore >= 0) {
+                    team2Score.text = newScore.toString()
+                }
             }
         }
     }
